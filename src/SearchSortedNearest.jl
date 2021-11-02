1147a53ffa899295a362e0b7a8e45a8decc88a61
joshday <emailjoshday@gmail.com>
2021-11-02 10:08:58 -0400

    Files generated by PkgTemplates

---
+++ src/SearchSortedNearest.jl
@@ -0,0 +1,5 @@
+module SearchSortedNearest
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SearchSortedNearest
+using Test
+
+@testset "SearchSortedNearest.jl" begin
+    # Write your tests here.
+end
