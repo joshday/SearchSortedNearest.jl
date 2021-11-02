9c855355e9b0735dbdc0018e53c27636590acf0b
joshday <emailjoshday@gmail.com>
2021-11-02 10:56:22 -0400

    first commit

--- src/SearchSortedNearest.jl
+++ src/SearchSortedNearest.jl
@@ -1,5 +1,28 @@
 module SearchSortedNearest
 
-# Write your package code here.
+export searchsortednearest
+
+# from: https://discourse.julialang.org/t/findnearest-function/4143/5
+"""
+    searchsortednearest(a, x; by=<transform>, lt=<comparison>, rev=false)
+
+Find the index of (sorted) collection `a` that is nearest (smallest absolute difference) to `x`.  
+Ties go to the smallest index.
+
+# Examples 
+
+    using SearchSortedNearest
+
+    searchsortednearest(1:10, 1.1) == 1
+    searchsortednearest(1:10, 1.9) == 2
+"""
+function searchsortednearest(a, x; by=identity, lt=isless, rev=false)
+    idx = searchsortedfirst(a, x; by, lt, rev)
+    idx == 1 && return idx
+    idx > length(a) && return length(a)
+    a[idx] == x && return idx 
+    idx = lt(abs(by(a[idx]) - by(x)), abs(by(a[idx - 1]) - by(x))) ? idx : idx - 1
+    return rev ? length(x) - idx + 1 : idx
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using SearchSortedNearest
 using Test
 
 @testset "SearchSortedNearest.jl" begin
-    # Write your tests here.
+    @testset "Range" begin
+        for rng in [1:10, (1:10) .+ 0.4, (1:10) .+ 0.5, 1.0:10.0], item in rng 
+            searchsortednearest(rng, item) == round(Int, item)
+        end
+        searchsortednearest(1:10, 0) == 1
+        searchsortednearest(1:10, 11) == 10
+        searchsortednearest(1:10, 0, rev=true) == 10
+        searchsortednearest(1:10, 11, rev=true) == 1
+    end
+    @testset "By" begin 
+        rng = map(x -> x => rand(), 1:10)
+        for item in rng 
+            searchsortednearest(rng, item, by=first) == first(item)
+            searchsortednearest(rng, item, by=first, rev=true) == 11 - first(item)
+        end
+    end
 end
