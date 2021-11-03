9ad8cfad9aa5ed6a50cb56e184a4b5780315207f
joshday <emailjoshday@gmail.com>
2021-11-03 10:36:11 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using SearchSortedNearest
 using Test
 
 @testset "SearchSortedNearest.jl" begin
-    @testset "Range" begin
+    @testset "Range checks" begin
         for rng in [1:10, (1:10) .+ 0.4, (1:10) .+ 0.5, 1.0:10.0], item in rng 
             @test searchsortednearest(rng, item) == floor(Int, item)
         end
@@ -11,18 +11,28 @@ using Test
         @test searchsortednearest(1:10, 0, rev=true) == 10
         @test searchsortednearest(1:10, 11, rev=true) == 1
     end
-    @testset "By" begin 
+    @testset "by" begin 
         struct StructWithoutOrder end
         rng = map(x -> x => StructWithoutOrder(), 1:10)
         for item in rng 
             @test searchsortednearest(rng, item, by=first) == first(item)
             @test searchsortednearest(rng, item, by=first, rev=true) == 11 - first(item)
         end
+        a = ["a", "aa", "aaa", "aaaa"]
+        x = "aaa"
+        @test searchsortedfirst(a, x, by=length) == 3
     end
-    for _ in 1:100
-        a = sort!(randn(100))
-        x = randn()
-        @test searchsortednearest(a, x) == argmin(abs.(a .- x))
-        @test searchsortednearest(a, x, rev=true) == argmin(abs.(reverse(a) .- x))
+    @testset "lt" begin 
+        a = 1:10
+        x = NaN
+        @test searchsortednearest(a, x, lt=isless) == 10
+        @test searchsortednearest(a, x, lt=<) == 1
+    end
+    @testset "random" begin
+        for _ in 1:100
+            a, x = sort!(randn(100)), randn()
+            @test searchsortednearest(a, x) == argmin(abs.(a .- x))
+            @test searchsortednearest(a, x, rev=true) == argmin(abs.(reverse(a) .- x))
+        end
     end
 end
